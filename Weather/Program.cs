4d1f15c6e72348cfb9ad82849bf4daa4c2d255d1
Paul Gearo <PaulGearo@gmail.com>
2019-02-15 09:01:22 -0600

    Add project files.

---
+++ Weather/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
