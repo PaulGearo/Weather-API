ad4172b9f04f0e417c09fb95899ad5e1df671b54
Paul Gearo <PaulGearo@gmail.com>
2019-03-18 09:34:28 -0500

    working temp display of Zip code

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
-namespace Weather
+namespace ChuckNorrisApi
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string userWeather = GetUserWeather();
+
+            double temp = TempConv(userWeather);
+            Console.WriteLine(temp);
+
+            Console.ReadLine();
         }
+
+        public static string ZipCode()
+        {
+            Console.WriteLine("What's your zip code?");
+            string zipCode = Console.ReadLine();
+            return zipCode;
+        }
+
+        static string GetUserWeather()
+        {
+            string AppSettings = File.ReadAllText("AppSetting.Dev.Json");
+            string appId = JObject.Parse(AppSettings)["appId"].ToString();
+
+            string zipCode = ZipCode();
+            WebClient webClient = new WebClient();
+            string apiResponce = webClient.DownloadString($"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},us&appid={appId}");
+            string weather = JObject.Parse(apiResponce)["main"]["temp"].ToString();
+            return weather;
+        }
+
+        public static double TempConv(string temp)
+        {
+            double tempKel = double.Parse(temp);
+            double tempF = 1.8 * (tempKel - 273) + 32;
+            return tempF;
+        }
+
     }
 }
