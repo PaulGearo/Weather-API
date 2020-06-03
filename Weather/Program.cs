53ef850d327c90261c39dc48518e999792fa0bc5
Paul Gearo <PaulGearo@gmail.com>
2020-06-02 21:50:44 -0400

    added feature to enter multiple zip codes

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -11,24 +11,48 @@ namespace ChuckNorrisApi
         {
 
             string userWeather = GetUserWeather();
-
             double temp = TempConv(userWeather);
-            Console.WriteLine(temp);
+            Console.WriteLine(temp + "°F");
 
-            Console.ReadLine();
+            bool isYes = YesOrNO();
+    
+            while (isYes)
+            {
+                userWeather = GetUserWeather();
+                temp = TempConv(userWeather);
+                Console.WriteLine(temp + "°F");
+            }
+
+            return;
         }
 
-        public static string ZipCode()
+        //this method is to gather the users Y/N answers, and to insure they answer as such
+        public static bool YesOrNO()
         {
-            Console.WriteLine("What's your zip code?");
-            string zipCode = Console.ReadLine();
-            return zipCode;
+            Console.WriteLine("Would you like to enter another?");
+            string userResponse = Console.ReadLine();
+            userResponse = userResponse.ToUpper();
+
+            while (userResponse != "YES" && userResponse != "NO")
+            {
+                Console.WriteLine("Please answer: Yes or No\n");
+                userResponse = Console.ReadLine();
+                userResponse = userResponse.ToUpper();
+            }
+
+            if (userResponse == "YES")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         //This method gets the API from OpenWeathermap.com 
         static string GetUserWeather()
         {
-            //
             string AppSettings = File.ReadAllText("AppSetting.Dev.Json");
             string appId = JObject.Parse(AppSettings)["appId"].ToString();
 
@@ -39,6 +63,13 @@ namespace ChuckNorrisApi
             string apiResponce = webClient.DownloadString($"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},us&appid={appId}");
             string weather = JObject.Parse(apiResponce)["main"]["temp"].ToString();
             return weather;
+        }
+
+        public static string ZipCode()
+        {
+            Console.WriteLine("What's your zip code?");
+            string zipCode = Console.ReadLine();
+            return zipCode;
         }
 
         //This method coverts Kelvin to fahrenheit 
