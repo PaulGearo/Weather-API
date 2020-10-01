4d220f3438d8ba28afa793720e1dc01510a4949a
Paul Gearo <PaulGearo@gmail.com>
2020-10-01 10:20:44 -0400

    Added HelperMethods file for organization

---
+++ Weather/HelperMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather
+{
+    public class HelperMethods
+    {
+        public static string GetZipCode()
+        {
+            Console.WriteLine("What's your zip code?");
+            string zipCode = Console.ReadLine();
+            return zipCode;
+        }
+
+        public static double TempConv(string temp)
+        {
+            double tempKel = double.Parse(temp);
+            double tempF = 1.8 * (tempKel - 273) + 32;
+            return tempF;
+        }
+
+        public static bool YesOrNO()
+        {
+            Console.WriteLine("Would you like to enter another?\n" + "Please enter yes or no");
+            string userResponse = Console.ReadLine();
+            userResponse = userResponse.ToUpper();
+
+            while (userResponse != "YES" && userResponse != "NO")
+            {
+                Console.WriteLine("Please answer: Yes or No\n");
+                userResponse = Console.ReadLine();
+                userResponse = userResponse.ToUpper();
+            }
+
+            if (userResponse == "YES")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- Weather/Program.cs
+++ Weather/Program.cs
@@ -3,82 +3,47 @@ using System.Net;
 using Newtonsoft.Json.Linq;
 using System.IO;
 
-namespace ChuckNorrisApi
+namespace Weather
 {
-    class Program
+    class GetWeather
     {
-        static void Main(string[] args)
+        public GetWeather()
         {
 
-            string userWeather = GetUserWeather();
-            double temp = TempConv(userWeather);
+        }
+
+        static void Main(string[] args)
+        {
+            string userWeather = RetrieveUserWeather();
+            double temp = HelperMethods.TempConv(userWeather);
             Console.WriteLine(temp + "°F");
 
-            bool isYes = YesOrNO();
+            bool isYes = HelperMethods.YesOrNO();
     
             while (isYes)
             {
-                userWeather = GetUserWeather();
-                temp = TempConv(userWeather);
+                userWeather = RetrieveUserWeather();
+                temp = HelperMethods.TempConv(userWeather);
                 Console.WriteLine(temp + "°F");
             }
 
             return;
         }
 
-        //this method is to gather the users Y/N answers, and to insure they answer as such
-        public static bool YesOrNO()
-        {
-            Console.WriteLine("Would you like to enter another?");
-            string userResponse = Console.ReadLine();
-            userResponse = userResponse.ToUpper();
-
-            while (userResponse != "YES" && userResponse != "NO")
-            {
-                Console.WriteLine("Please answer: Yes or No\n");
-                userResponse = Console.ReadLine();
-                userResponse = userResponse.ToUpper();
-            }
-
-            if (userResponse == "YES")
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        //This method gets the API from OpenWeathermap.com 
-        static string GetUserWeather()
+        
+        static string RetrieveUserWeather()
         {
             string AppSettings = File.ReadAllText("AppSetting.Dev.Json");
             string appId = JObject.Parse(AppSettings)["appId"].ToString();
 
-            string zipCode = ZipCode();
+            string zipCode = HelperMethods.GetZipCode();
 
             //
             WebClient webClient = new WebClient();
             string apiResponce = webClient.DownloadString($"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},us&appid={appId}");
-            string weather = JObject.Parse(apiResponce)["main"]["temp"].ToString();
+            var weather = JObject.Parse(apiResponce)["main"]["temp"].ToString();
+
             return weather;
         }
-
-        public static string ZipCode()
-        {
-            Console.WriteLine("What's your zip code?");
-            string zipCode = Console.ReadLine();
-            return zipCode;
-        }
-
-        //This method coverts Kelvin to fahrenheit 
-        public static double TempConv(string temp)
-        {
-            double tempKel = double.Parse(temp);
-            double tempF = 1.8 * (tempKel - 273) + 32;
-            return tempF;
-        }
-
     }
 }
